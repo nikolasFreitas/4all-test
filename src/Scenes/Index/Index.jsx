1c773e46ed0a5e612260f737e5e7a2757ba582d7
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-13 02:33:35 -0300

    feat: Make this scene dynamic  - Make the list of Ids income with webservice  - Check if the list is arrived from server or not

--- src/Scenes/Index/Index.jsx
+++ src/Scenes/Index/Index.jsx
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import IdFetch from '../../Services/Webservice/4allHost/Id-consult.jsx';
 
 class Index extends Component {
     constructor (props) {
         super(props);
-        this.state = {
 
+        this.state = {
+            options : {},
+            clickedId : null,
+            received : false,
         }
     }
 
+    componentDidMount() {
+        IdFetch().then(([data]) => {
+            this.setState({
+                received : true,
+                options : data
+            });
+        });
+    }
 
     render() {
-        return(
-            <div className="container container--middle">
-                <h1>Escolha uma das opções</h1>
-                <ul className="list list--vertical list--user-id">
-                    <li className="list-item list-item--user-id">place holder</li>
-                    <li className="list-item list-item--user-id">place holder</li>
-                    <li className="list-item list-item--user-id">place holder</li>
-                    <li className="list-item list-item--user-id">place holder</li>
-                </ul>
-            </div>
+         if (this.state.received) {
+            return(
+                <div className="container container--middle">
+                    <h1>Escolha uma das opções</h1>
+                    <ul className="list list--vertical list--user-id">
+                        {
+                            this.state.options.lista.map((option, index) => {
+                                return (
+                                    <li className="list-item list-item--user-id" key={index}>{option}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                 </div>
 
-        );
+            )
+         } else {
+            return(
+                <div className="container container--middle">
+                    <h1> Aguardando receber informação</h1>
+                </div>
+            );
+         }
     }
 }
 
