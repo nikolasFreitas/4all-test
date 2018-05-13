cf55b18c0fb8b07933c776d12c8c19096c882d3e
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-13 15:48:18 -0300

    feat: Adapt the scene to receive errors from fetch

--- src/Scenes/Index/Index.jsx
+++ src/Scenes/Index/Index.jsx
@@ -9,39 +9,56 @@ class Index extends Component {
             options : {},
             clickedId : null,
             received : false,
+            response : false
         }
     }
 
     componentDidMount() {
-        IdFetch().then(([data]) => {
-            this.setState({
-                received : true,
-                options : data
+        IdFetch()
+            .then( data => {
+                if (data) {
+                    this.setState({
+                        received : true,
+                        response : true,
+                        options : data
+                    });
+                } else {
+                    console.log(data);
+                    this.setState({
+                        received : true
+                    })
+                }
             });
-        });
     }
 
     render() {
-         if (this.state.received) {
-            return(
-                <div className="container container--middle">
-                    <h1>Escolha uma das opções</h1>
-                    <ul className="list list--vertical list--user-id">
+        if (this.state.received) {
+            if (this.state.response) {
+                return(
+                    <div className="container container--middle">
+                        <h1>Escolha uma das opções</h1>
+                        <ul className="list list--vertical list--user-id">
                         {
-                            this.state.options.lista.map((option, index) => {
-                                return (
-                                    <li className="list-item list-item--user-id" key={index}>{option}</li>
-                                )
-                            })
+                         this.state.options.lista.map((option, index) => {
+                             return (
+                                 <li className="list-item list-item--user-id" key={index}>{option}</li>
+                             )
+                         })
                         }
-                    </ul>
-                 </div>
-
-            )
-         } else {
+                        </ul>
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="container container--middle">
+                        <h1> Infelizmente não foi possível conectar ao nosso servidor</h1>
+                    </div>
+                )
+            }
+        } else {
             return(
                 <div className="container container--middle">
-                    <h1> Aguardando receber informação</h1>
+                    <h1> Buscando informação do servidor</h1>
                 </div>
             );
          }
