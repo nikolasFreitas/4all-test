145e75621bd7b8480ce5687ac00b8d0def9f3a7b
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-13 15:51:09 -0300

    test: Add the correct mock

--- src/Scenes/Index/Index.test.jsx
+++ src/Scenes/Index/Index.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Index from './Index.jsx';
 
+jest.mock('../../Services/Webservice/4allHost/Id-consult.jsx');
+
 describe('Block of tests of Index scene', () => {
     it('Check if Index scene is defined', () => {
         expect(<Index />).toBeDefined();
