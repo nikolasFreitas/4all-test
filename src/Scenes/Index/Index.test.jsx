665ac07ab4f3f755b058b1ce515a2ebdc7a8ed2c
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-13 20:50:30 -0300

    test: Make async test on Index

--- src/Scenes/Index/Index.test.jsx
+++ src/Scenes/Index/Index.test.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Index from './Index.jsx';
 
+import {configure, shallow, mount, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 jest.mock('../../Services/Webservice/4allHost/Id-consult.jsx');
 
 describe('Block of tests of Index scene', () => {
-    it('Check if Index scene is defined', () => {
-        expect(<Index />).toBeDefined();
+
+
+
+    it('check if Index states are the default', () => {
+        const wrapper =  shallow(<Index />);
+        expect(wrapper.state('received')).toBe(false);
+        expect(wrapper.state('response')).toBe(false);
+        expect(wrapper.state('options')).toEqual({});
+    });
+
+    test('check if Index states change when return the promise',async () => {
+        const wrapper = await shallow(<Index />);
+
+        expect(wrapper.state('received')).toBe(true);
+        expect(wrapper.state('response')).toBe(true);
+
     });
 });
