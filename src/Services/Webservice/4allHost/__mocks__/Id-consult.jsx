f13d1465c8c1ecbab37ea0eec5c7783c96b85e09
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-13 21:48:37 -0300

    refactor: Simplify Promise made in mock and make the return equal fetch would

--- src/Services/Webservice/4allHost/__mocks__/Id-consult.jsx
+++ src/Services/Webservice/4allHost/__mocks__/Id-consult.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 function getId(props) {
     const id = {
-        1: 'a',
-        2 : 4,
-        3 : 'das',
-        5 : 'things'
+        lista : [
+            1 : 'a',
+            2 : 4,
+            3 : 'das',
+            5 : 'things'
+        ]
     }
 
     return new Promise((resolve,reject) => {
-        process.nextTick(() => {
-            id[2]
-                ? resolve(id[2])
-                : reject({
-                    error: 'Não existe o ID:' + props.id
-                });
-        });
+        resolve(id)
     });
 }
 
