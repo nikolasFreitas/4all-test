d188771aab18f4b109997b5ab6f6c2b99e11789c
nikolas <nikolasbvf@gmail.com>
2018-05-14 20:12:16 -0300

    refactor: Encapsulate the nva-buttons inside it's own section

--- src/Scenes/Home/Home.jsx
+++ src/Scenes/Home/Home.jsx
@@ -16,12 +16,25 @@ class Home extends Component {
         return(
             <div className='container'>
                 <Header placeName='Porto Alegre, RS'/>
+
                 <section className='home-image-box'>
-                    <div className="favorite-icon"></div>
+                    <div className="favorite-icon"><i className="fa fa-star"></i></div>
                 </section>
-                <h2>Lorem</h2>
+
+                <section>
+                    <h2>Lorem</h2>
+                    <div className="content-box">
+                        <div className='tool-box'>
+                            <span className='nav-btn nav-btn--call'></span>
+                            <span className='nav-btn nav-btn--services'></span>
+                            <span className='nav-btn nav-btn--adress'></span>
+                            <span className='nav-btn nav-btn--comentary'></span>
+                            <span className='nav-btn nav-btn--favorite'></span>
+                        </div>
+                    </div>
+                </section>
             </div>
-            )
+        )
     }
 }
 
