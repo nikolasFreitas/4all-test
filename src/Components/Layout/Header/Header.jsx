da72770a070a42cf62b510068b2284b52c5870c0
nikolas <nikolasbvf@gmail.com>
2018-05-14 11:46:05 -0300

    feat: Initial component

---
+++ src/Components/Layout/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <header className='header'>
+                <div className='backward-icon'> </div>
+                <div className='header-place'>
+                    <span className='place-icon'></span>
+                    <span className='localization'></span>
+                </div>
+                <div className='search-icon'> </div>
+            </header>
+            )
+    }
+}
+
+export default Header
