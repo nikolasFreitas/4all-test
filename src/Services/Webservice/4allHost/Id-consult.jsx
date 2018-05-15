fbaf002ef2212c1dada428e267ef95871de925c1
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-15 16:27:48 -0300

    fix: Conditional return of fetch result

--- src/Services/Webservice/4allHost/Id-consult.jsx
+++ src/Services/Webservice/4allHost/Id-consult.jsx
@@ -2,13 +2,14 @@ async function getId(props) {
     const url = 'http://dev.4all.com:3003';
     const page = 'tarefa';
 
-    try {
         const theFetch = await fetch(`${url}/${page}`);
 
-        return theFetch.json();
-    } catch (err) {
-        return false
-    }
+        if (theFetch.ok) {
+            return theFetch.json();
+        } else {
+            return false
+        }
+
 }
 
 export default getId;
