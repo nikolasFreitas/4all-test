83581898c0b5733dd75b8a781fb4aaf5f6f3ff34
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-15 12:23:27 -0300

    feat: Add text to buttons

--- src/Scenes/Home/Home.jsx
+++ src/Scenes/Home/Home.jsx
@@ -27,22 +27,27 @@ class Home extends Component {
                         <div className='tool-box'>
                             <span className='nav-btn nav-btn--call'>
                                 <i className="fa fa-phone"></i>
+                                <span className="nav-btn__legend">Ligar</span>
                             </span>
 
                             <span className='nav-btn nav-btn--services'>
                                 <i className="fa fa-diamond"></i>
+                                <span className="nav-btn__legend">Serviços</span>
                             </span>
 
                             <span className='nav-btn nav-btn--adress'>
                                 <i className="fa fa-map-marker"></i>
+                                <span className="nav-btn__legend">Endereço</span>
                             </span>
 
                             <span className='nav-btn nav-btn--comentary'>
                                 <i className="fa fa-comments"></i>
+                                <span className="nav-btn__legend">Comentários</span>
                             </span>
 
                             <span className='nav-btn nav-btn--favorite'>
                                 <i className="fa fa-star"></i>
+                                <span className="nav-btn__legend">Favoritos</span>
                             </span>
                         </div>
 
