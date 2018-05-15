568504672458e9dac3acf6e5dfee4778fec9417d
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-15 12:53:16 -0300

    feat: Import Enzyme

--- src/Services/Webservice/4allHost/Id-consult.test.jsx
+++ src/Services/Webservice/4allHost/Id-consult.test.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Consult from './Id-consult.jsx';
 
+import {configure, shallow, mount, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 describe('Consult ID of 4all REST', () => {
     test('Check if Consult exists', () => {
         expect(Consult).toBeDefined();
