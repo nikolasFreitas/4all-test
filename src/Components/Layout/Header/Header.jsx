9f2a8b2710ffee49cbb9dc95a1c20f965e538509
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-15 19:45:11 -0300

    feat: Insert icons to Header component

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -8,12 +8,16 @@ class Header extends Component {
     render() {
         return(
             <header className='header'>
-                <div className='header-icon header-icon--backward'> </div>
+                <div className='header-icon header-icon--backward'>
+                    <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                </div>
                 <div className='header-place'>
                     <span className='header-place__place-icon'><i className="fa fa-map-marker"></i></span>
                     <span className='localization'>{this.props.placeName}</span>
                 </div>
-                <div className='header-icon header-icon--search'> </div>
+                <div className='header-icon header-icon--search'>
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </div>
             </header>
             )
     }
