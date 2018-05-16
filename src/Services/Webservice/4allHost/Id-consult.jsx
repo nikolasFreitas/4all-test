e47a9e3e1195e060c7fde010e859b9d9cfc90b26
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-16 01:04:15 -0300

    style: Id-consult.jsx identation

--- src/Services/Webservice/4allHost/Id-consult.jsx
+++ src/Services/Webservice/4allHost/Id-consult.jsx
@@ -2,14 +2,13 @@ async function getId(props) {
     const url = 'http://dev.4all.com:3003';
     const page = 'tarefa';
 
-        const theFetch = await fetch(`${url}/${page}`);
+    const theFetch = await fetch(`${url}/${page}`);
 
-        if (theFetch.ok) {
-            return theFetch.json();
-        } else {
-            return false
-        }
-
+    if (theFetch.ok) {
+        return theFetch.json();
+    } else {
+        return false
+    }
 }
 
 export default getId;
