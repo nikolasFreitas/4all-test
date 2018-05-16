04796bcb240cbec6718acedc0661374bbd09f09d
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-16 00:36:01 -0300

    refactor: Increase default map zoom on Maps

--- src/Components/GoogleMaps/Maps.jsx
+++ src/Components/GoogleMaps/Maps.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {withGoogleMap, GoogleMap, Marker, withScriptjs } from "react-google-maps"
+import {withGoogleMap, GoogleMap, Marker, withScriptjs, InfoWindow } from "react-google-maps"
 
 const GoogleMaps = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
-      defaultZoom={8}
+      defaultZoom={15}
       defaultCenter={{ lat: props.lat, lng: props.long }}
     >
-    <Marker
-        position={{ lat: props.lat, lng: props.long }}
-    />
+        <Marker
+            position={{ lat: props.lat, lng: props.long }}
+        />
     </GoogleMap>
 ));
 
