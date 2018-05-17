6ef7833c1ec0f9979e36539f0e7261d5cff80b71
nikolas bernardes vieira de freitas <nikolasbvf@gmail.com>
2018-05-17 13:31:19 -0300

    refactor: Removed history from router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './Assets/css/general.css';
 import Home from './Scenes/Home/Home';
+import Index from './Scenes/Index/Index'
+
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
+
 
 class App extends Component {
     render() {
         return (
-            <Home id='ultimo'/>
+            <Router>
+                <Switch>
+                    <Route exact path='/'exact strict render={(props) => <Index {...props} />}/>
+                    <Route path='/home/:id'exact strict render={(props) => <Home {...props} />}/>
+                </Switch>
+            </Router>
         );
     }
 }
